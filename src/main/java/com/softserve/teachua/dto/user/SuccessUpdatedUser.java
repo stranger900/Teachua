ee254099bda2f58a738695c61b3b1d1b7d2d4649
Administrator <admin@example.com>
2021-02-24 20:37:45 +0200

    TeachUA

---
+++ src/main/java/com/softserve/teachua/dto/user/SuccessUpdatedUser.java
@@ -0,0 +1,21 @@
+package com.softserve.teachua.dto.user;
+
+import com.softserve.teachua.dto.marker.Convertible;
+import com.softserve.teachua.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class SuccessUpdatedUser implements Convertible {
+
+    private String name;
+    private Long id;
+    private String email;
+    private String roleName;
+
+}
---
+++ src/main/java/com/softserve/teachua/dto/user/UserProfile.java
@@ -0,0 +1,30 @@
+package com.softserve.teachua.dto.user;
+
+import com.softserve.teachua.dto.marker.Convertible;
+import com.softserve.teachua.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotEmpty;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class UserProfile implements Convertible {
+
+    private Long id;
+
+    @NotEmpty
+    private String email;
+
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String password;
+
+    private String roleName;
+}
